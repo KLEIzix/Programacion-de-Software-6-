230ced0b14c364951993baaaae589144eb670ebb
Diego-Alejo <bstivenacetobon888@gmail.com>
2025-08-27 06:57:08 -0500

    cambio en desarrollo 6:54, 27/08/25

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var persona = new Compañeros();
+Compañeros persona 1 = new Personas();
+Personas persona 2 = new Estudiantes();
+
+persona.Id = 1;
+persona.Nombre = "Pepito Perez";
+persona.Actuvo = true;
+persona.Salario = 1400000.0m;
+persona.tipo = new Tipos()(Id = 1, Nombre = "Casado");
+Persona:VideoJuegos = new List<VideoJuegos>;
+persona.VideoJuegos.Add (new VideoJuegos() (Id = 1 Nombre = "Zelda"))
+persona.VideoJuegos.Add(new VideoJuegos()(Id = 2 Nombre = "Tetris"));
+
+Console.WriteLine(persona.Nombre);
+Console.WriteLIne(persona.tipo.Nombre);
+
+foreach (var elemento in persona.VideoJuegoss)
+{
+    Console.WriteLine(elemento.Nombre);
+}
+
+
+public class Tipos
+{
+    public int Id = 0;
+    public String? Nombre = " ";
+}
+
+public class VideoJuegos
+{
+    public int Id = 0;
+    public string? Nombre = " ";
+}
+
+public class Estudiantes : Compañeros
+{
+    public int Id = 0; 
+}
+
+
+public class Compañeros
+{
+    public int Id = 0;
+    public String? Nombre = " ";
+    public DateTime Fecha = DateTime.Now;
+    public bool Activo = false;
+    public decimal Salario = 0.0m;
+    public Tipos tipo = new Tipos();
+    public List<VideoJuegos> VideoJuegos = new List<VideoJuegos>();
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Programacion-de-Software-6-
 clase del 21/08/2025
+
+Cambio en desarrollo 6:54
